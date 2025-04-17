6ca5d5a8599e2c63a8028f77ba4811f69ecbe4e4
Axelo-n <mkev46p@gmail.com>
2025-04-17 08:28:23 +0700

    added best agent, improved navbar

--- JS/palorant.js
+++ JS/palorant.js
@@ -47,16 +47,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     const cards = document.querySelectorAll(".agents-card");
+    const bestAgent = document.querySelector(".best-agent");
 
-        cards.forEach(card => {
-            card.addEventListener("mouseenter", () => {
-                cards.forEach(c => {
-                    if (c !== card) c.classList.add("dimmed");
-                });
+    cards.forEach(card => {
+        card.addEventListener("mouseenter", () => {
+            // Tambahin dimmed ke semua card kecuali yang di-hover
+            cards.forEach(c => {
+                if (c !== card) c.classList.add("dimmed");
             });
 
-            card.addEventListener("mouseleave", () => {
-                cards.forEach(c => c.classList.remove("dimmed"));
-            });
+            // Kalau yang di-hover BUKAN chamber, kasih dimmed ke .best-agent
+            if (card.id !== "chamber") {
+                bestAgent.classList.add("dimmed");
+            }
         });
+
+        card.addEventListener("mouseleave", () => {
+            // Reset semua
+            cards.forEach(c => c.classList.remove("dimmed"));
+            bestAgent.classList.remove("dimmed");
+        });
+    });
+
 });
